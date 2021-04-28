f70c89627c199b1e25fe0cefd437698f0bd585ec
John Huang <huang@huang-a03.vmware.com>
2021-04-27 22:39:24 -0700

    mutationn

--- app/app/schema.py
+++ app/app/schema.py
@@ -4,5 +4,7 @@ import tracks.schema
 class Query(tracks.schema.Query, graphene.ObjectType):
     pass
 
+class Mutation(tracks.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
