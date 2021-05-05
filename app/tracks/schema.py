8698847e57a44ada7181af2493a32dc6dc5dc1e5
John Huang <huang@huang-a03.vmware.com>
2021-05-05 07:01:00 -0700

    GraphQLError

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from graphql import GraphQLError
 
 from .models import Track, Like
 from users.schema import UserType
@@ -18,7 +19,7 @@ class Query(graphene.ObjectType):
 
     def resolve_tracks(self, info):
         return Track.objects.all()
-        
+
     def resolve_likes(self, info):
         return Like.objects.all()
 
@@ -33,7 +34,7 @@ class CreateTrack(graphene.Mutation):
     def mutate(self, info, title, description, url):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Log in to add a track')
+            raise GraphQLError('Log in to add a track')
         track = Track(title=title, description=description, url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
@@ -52,7 +53,7 @@ class UpdateTrack(graphene.Mutation):
         track = Track.objects.get(id=track_id)
 
         if track.posted_by != user:
-            raise Exception('Not permitted to update this track.')
+            raise GraphQLError('Not permitted to update this track.')
 
         track.title = title
         track.description = description
@@ -72,7 +73,7 @@ class DeleteTrack(graphene.Mutation):
         track = Track.objects.get(id=track_id)
 
         if track.posted_by != user:
-            raise Exception('Not permitted to update this track.')
+            raise GraphQLError('Not permitted to update this track.')
 
         track.delete()
         return DeleteTrack(track_id=track_id)
@@ -88,11 +89,11 @@ class CreateLike(graphene.Mutation):
     def mutate(self, info, track_id):
         user = info.context.user
         if user.is_anonymous:
-            raise Exception('Login to like tracks')
+            raise GraphQLError('Login to like tracks')
 
         track = Track.objects.get(id=track_id)
         if not track:
-            raise Exception('Cannot find track with given track id')
+            raise GraphQLError('Cannot find track with given track id')
 
         Like.objects.create(
             user=user,
