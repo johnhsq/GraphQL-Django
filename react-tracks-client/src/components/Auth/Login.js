2b874d07bc06ac1cdacf5d0d13678914f5e8e78b
johnhsq <johnhsq@yahoo.com>
2021-05-25 07:51:01 -0700

    Auth

--- react-tracks-client/src/components/Auth/Login.js
+++ react-tracks-client/src/components/Auth/Login.js
@@ -16,10 +16,11 @@ const Login = ({ classes, setNewUser }) => {
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
-  const handleSubmit = async (event, tokenAuth) =>{
+  const handleSubmit = async (event, tokenAuth, client) =>{
     event.preventDefault()
     const res = await tokenAuth()
     localStorage.setItem('authToken', res.data.tokenAuth.token)
+    client.writeData({data: {isLoggedIn: true}})
   } 
 
   return (
@@ -33,9 +34,9 @@ const Login = ({ classes, setNewUser }) => {
         </Typography>
         <Mutation mutation={LOGIN_MUTATION}
           variables={{username, password}} >
-          {(tokenAuth, {loading, error}) => {
+          {(tokenAuth, {loading, error, called, client }) => {
             return (
-              <form onSubmit={event => handleSubmit(event, tokenAuth)} className={classes.form}>
+              <form onSubmit={event => handleSubmit(event, tokenAuth, client)} className={classes.form}>
                 <FormControl margin="normal" required fullWidth>
                   <InputLabel htmlFor="usrname">Username</InputLabel>
                   <Input id="username" onChange={event => setUsername(event.target.value)} />
