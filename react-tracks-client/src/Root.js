ed1d7f71cc3750424603090efd57251f58d579a5
johnhsq <johnhsq@yahoo.com>
2021-05-26 08:07:01 -0700

    routing

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,5 +1,10 @@
 import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import withRoot from "./withRoot";
+import App from './pages/App'
+import Profile from './pages/Profile'
+import Header from './components/Shared/Header'
 
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
@@ -10,7 +15,17 @@ const Root = () => (
             if (loading) return <div>Loading</div>
             if (error) return <div>Error</div>
 
-            return <div>{JSON.stringify(data)}</div>
+            return (
+                <Router>
+                    <>
+                    <Header />
+                    <Switch>
+                        <Route exact path="/" component={App} />
+                        <Route path="/profile/:id" component={Profile} />
+                    </Switch>
+                    </>
+                </Router>
+            )
         }}
     </Query>
 )
