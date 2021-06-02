e228fce603febfa8473e344f7625916b30ac5266
johnhsq <johnhsq@yahoo.com>
2021-06-02 09:18:47 -0700

    Loading

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -5,6 +5,8 @@ import withRoot from "./withRoot";
 import App from './pages/App'
 import Profile from './pages/Profile'
 import Header from './components/Shared/Header'
+import Loading from './components/Shared/Loading'
+import Error from './components/Shared/Error'
 
 import { Query } from 'react-apollo'
 import { gql } from 'apollo-boost'
@@ -12,8 +14,8 @@ import { gql } from 'apollo-boost'
 const Root = () => (
     <Query query={ME_QUERY}>
         {({data, loading, error}) => {
-            if (loading) return <div>Loading</div>
-            if (error) return <div>Error</div>
+            if (loading) return <Loading />
+            if (error) return <Error error={error} />
             const currentUser = data.me
 
             return (
